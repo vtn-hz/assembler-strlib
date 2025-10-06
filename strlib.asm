4e8f28cfe6f84ee1a1a0525c0e9f9af453bf4b29
vtn-hz <juaniyvalen1234@gmail.com>
2025-10-05 22:05:41 -0300

    feat: add SCMP and SCAT functions to string library

--- strlib.asm
+++ strlib.asm
@@ -24,6 +24,7 @@ SLEN:   PUSH BP
                     POP BP
 RET
 
+
 ;   SCPY
 ;   usage:
 ;       PUSH destino
@@ -57,3 +58,73 @@ SCPY:   PUSH BP
 RET
                 
     
+;   SCMP
+;   usage:
+;       PUSH origen
+;       PUSH compated
+;       CALL SCMP @retrieve EAX : diference
+;       ADD SP, 8
+;  
+SCMP:   PUSH BP 
+        MOV BP, SP
+
+        PUSH EBX
+        PUSH ECX
+
+        MOV EBX, [BP + 12]
+        MOV ECX, [BP + 8 ]
+
+        SCMP_LOOP:      MOV EAX, b[EBX]
+                        SUB EAX, b[ECX]
+
+                        CMP b[EBX], 0
+                        JZ SCMP_FIN
+                        CMP b[ECX], 0
+                        JZ SCMP_FIN 
+
+                        ADD EBX, 1 
+                        ADD ECX, 1
+        JMP SCMP_LOOP
+
+        SCMP_FIN:       POP ECX 
+                        POP EBX
+
+                        MOV SP, BP
+                        POP BP
+RET
+
+;   SCAT
+;   usage:
+;       PUSH origen
+;       PUSH concated
+;       CALL SCMP @retrieve EAX : origen
+;       ADD SP, 8
+;  
+SCAT:   PUSH BP 
+        MOV BP, SP
+
+        PUSH EBX
+        PUSH ECX
+
+        MOV EBX, [BP + 12]
+        MOV ECX, [BP + 8 ]
+
+        SCAT_FIND0:     CMP b[EBX], 0
+                        JZ SCAT_FIND0
+                        ADD EBX, 1
+        JMP SCAT_FIND0
+
+        SCAT_STARTCAT:  MOV b[EBX], b[ECX]
+
+                        CMP b[ECX], 0
+                        JZ SCAT_FIN
+
+                        ADD ECX, 1
+        JMP SCAT_STARTCAT
+
+        SCAT_FIN:       POP ECX
+                        POP EBX
+                        
+                        MOV SP, BP
+                        POP BP 
+RET
